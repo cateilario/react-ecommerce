af110c160d1cc5bafad6e02ae7fbf77b1bccfc3f
Cate <caterinailariopaz@alumnos.tuniversformacion.es>
2024-03-04 10:57:23 +0100

    estilos y logica login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { API_URL } from "../../constants/env";
 import { setToken } from "../../helpers/auth";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import logoImage from "./../../assets/imgs/logo.svg";
 
 const Login = () => {
 
@@ -35,13 +36,51 @@ const Login = () => {
   }
 
   return (
-    <div className="pt-16 max-w-256 m-auto">
-        <form onSubmit={handleSubmit}>
-        <input type="email" placeholder="Email" required/>
-        <input type="password" placeholder="Contraseña" required/>
-        <button type="submit">Iniciar sesión</button>
-        </form>
-    </div>
+    <section className="w-full gradient-form bg-grey-200 md:h-screen">
+      <div className="container m-auto py-12 px-6 h-full">
+        <div className="flex justify-center items-center flex-wrap h-full text-gray-800">
+          <div className="xl:w-10/12">
+            <div className="block bg-white shadow-lg rounded-lg">
+              <div className="lg-flex lg:flex-wrap g-0">
+                <div className="lg:w-6/12 px-4 md:px-0">     
+                  <div className="md:p-12 md:mx-6">  
+                    <div className="text-center">
+                      <img className="m-auto w-48 mb-4 pt-4" src={logoImage} alt="" />
+                      <h1 className="font-semibold mt-1 mb-8 pb-1">Iniciar sesión</h1>
+                    </div>                    
+                    <form onSubmit={handleSubmit}>
+                      <div className="mb-4">
+                        <input className="p-1" type="email" placeholder="Correo electrónico" name="email" required/>
+                      </div>
+                      <div className="mb-4">
+                        <input className="p-1" type="password" placeholder="Contraseña" name="password" required/>
+                      </div>          
+                      <div className="text-center pt-1 mb-12 pb-1">
+                        <button className="bg-gradient w-full text-white rounded-full p-1" type="submit">
+                          Ingresar
+                        </button>
+                        <Link className="text-gray-500" to="/registro">
+                          ¿Deseas registrarte?
+                        </Link>
+                      </div>
+                      {error && (
+                        <p className="text-center p-2 bg-red-100 text-red-800">
+                          {error?.resp?.data?.data}
+                        </p>
+                      )}
+                    </form> 
+                  </div> 
+                </div>
+                <div className="bg-gradient lg:w-6/12 flex items-center lg:rounded-r-lg rounded-bl-lg-none">
+                  <span>Más que un e-commerce...</span>
+                  <h2>Somos una tienda en línea</h2>
+                </div>
+              </div> 
+            </div>
+          </div>         
+        </div>
+      </div>
+    </section>
   )
 }
 
